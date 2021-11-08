afe279de95f7492efcb7b070dc2590fd233fae0f
iDANDHER <idandher@outlook.com>
2021-11-07 18:24:16 -0600

    Add files via upload

---
+++ JavaTemplate.java
@@ -0,0 +1,11 @@
+import com.sap.aii.mapping.api.*:
+
+public class JavaTemplate extends AbstractTransformation{
+
+
+	public void transform(TransformationInput in, TransformationOutput out)throws StreamTransformationException{
+
+		//
+	}
+
+}
